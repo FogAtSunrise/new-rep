fb9f01a773ce6bb0d7264f5f7694a53d576906e2
Zhukova <55238935+AllenMiln0@users.noreply.github.com>
2020-01-09 16:05:53 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DOIT._1
+{
+    public partial class Enter : Form
+    {
+        public Enter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
